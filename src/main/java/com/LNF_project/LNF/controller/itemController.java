28062e1cc8103f7e7ab17492527a6396a44d650e
Khushnood <khushnoodahmed956@gmail.com>
2026-01-13 01:23:10 +0500

    Add item show, delete, found status and partial update endpoints

--- src/main/java/com/LNF_project/LNF/controller/itemController.java
+++ src/main/java/com/LNF_project/LNF/controller/itemController.java
@@ -2,9 +2,13 @@ package com.LNF_project.LNF.controller;
 
 import com.LNF_project.LNF.model.Item;
 import com.LNF_project.LNF.service.itemService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/item")
@@ -25,5 +29,32 @@ public class itemController {
     public List<Item> showAll() {
         return service.getAll();
     }
+
+    @GetMapping("/show/{id}")
+    public ResponseEntity<Item> getItemById(@PathVariable("id") Integer id){
+        Optional<Item> item = service.getById(id);
+        return item.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Item> DeleteById(@PathVariable Integer id){
+         service.deleteById(id);
+         return ResponseEntity.noContent().build();
+    }
+
+    @PatchMapping("/found/{id}")
+    public ResponseEntity<Item> updateFoundStatus(@PathVariable Integer id){
+        service.updateFound(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PatchMapping("/update/{id}")
+    public ResponseEntity<Item> updateInfo(@PathVariable Integer id, @RequestBody Map<String, Object> updates){
+        Item itemUpdated = service.updateInfo(id, updates);
+        return ResponseEntity.ok(itemUpdated);
+
+    }
+
+
 }
 
--- src/main/java/com/LNF_project/LNF/service/itemService.java
+++ src/main/java/com/LNF_project/LNF/service/itemService.java
@@ -2,14 +2,18 @@ package com.LNF_project.LNF.service;
 
 import com.LNF_project.LNF.model.Item;
 import com.LNF_project.LNF.repository.itemRepo;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.servlet.Servlet;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.*;
+
 @Service
 public class itemService {
     private final itemRepo itemRepo;
 
-    public itemService(itemRepo itemRepo){
+
+    public itemService(itemRepo itemRepo, Item item, Servlet servlet){
         this.itemRepo = itemRepo;
     }
 
@@ -19,5 +23,37 @@ public class itemService {
 
     public List<Item> getAll(){
         return itemRepo.findAll();
+    }
+
+    public Optional<Item> getById(Integer id) {
+        return itemRepo.findById(id);
+    }
+
+    public void deleteById(Integer id) {
+        if(!itemRepo.existsById(id)){
+            throw new EntityNotFoundException("Item not found");
+        }
+        itemRepo.deleteById(id);
+    }
+
+    public void updateFound(Integer id) {
+        Item item = itemRepo.findById(id).orElseThrow(() -> new RuntimeException("Item Not Found"));
+        item.setFound(true);
+        itemRepo.save(item);
+    }
+
+    public Item updateInfo(Integer id, Map<String, Object> updates) {
+        Item item = itemRepo.findById(id)
+                .orElseThrow(() -> new RuntimeException("Item not found"));
+        Set<String> allowedAttributes = Set.of("itemName","description", "ownerName","contactNo");
+        updates.keySet().retainAll(allowedAttributes);
+        if(updates.containsKey("itemName")) item.setItemName((String) updates.get("itemName"));
+        if(updates.containsKey("description")) item.setDescription((String) updates.get("description"));
+        if(updates.containsKey("ownerName")) item.setOwnerName((String) updates.get("ownerName"));
+        if(updates.containsKey("contactNo")) item.setContactNo((String) updates.get("contactNo"));
+
+        return itemRepo.save(item);
+
+
     }
 }
