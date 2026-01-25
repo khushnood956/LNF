3f3597823fc99a09c54e6bedea1dbf73e9a15b57
Khushnood <khushnoodahmed956@gmail.com>
2026-01-25 23:21:56 +0500

    feat: add responsive frontend for Lost & Found system and organized project structure

--- src/main/java/com/LNF_project/LNF/repository/ItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.LNF_project.LNF.repository;
-
-import com.LNF_project.LNF.model.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ItemRepository extends JpaRepository<Item, Integer> {
-}
